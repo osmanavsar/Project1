7f65201c54f7925a5349aacf837212ce78e9f916
osmanavsar <74041216+osmanavsar@users.noreply.github.com>
2021-01-10 01:15:51 +0300

    trial 2

--- djangoProject3/urls.py
+++ djangoProject3/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from djangoProject3.views import home
-from Healthbooster.views import search_form,search
+from Healthbooster.views import search_form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home),
     path('search_form/', search_form)
 ]
